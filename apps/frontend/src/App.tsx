0a53d6e376050284f2f0e14ccd58a0357d9b213f
Areco-Github <arecojorge2001@gmail.com>
2025-05-27 18:08:57 -0300

    nuevo componente Areco con con cálculo de reactancia capacitiva

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,8 +1,8 @@
 import { Sidebar } from './components'
 import { sidebarItems } from './components/Sidebar/sidebar.items'
 import AlexisComponent from './components/AlexisComponent/AlexisComponent'
-import WilsonComponent from './components/WilsonComponent/Wilsoncomponent'
-
+import ArecoComponent from './components/ArecoComponent/ArecoComponent'
+// import WilsonComponent from './components/WilsonComponent/Wilsoncomponent'
 
 function App() {
   return (
@@ -10,12 +10,11 @@ function App() {
       <div className='container-fluid p-0'>
         <div className="row">
           <div className="col">
-            <Sidebar items = {sidebarItems} />
+            <Sidebar items={sidebarItems} />
           </div>
           <div className="col">
-            <AlexisComponent a={2} b={2}/>
-            <WilsonComponent/>
-
+            <AlexisComponent a={2} b={2} />
+            <ArecoComponent frecuencia={60} capacitancia={0.000001} />
           </div>
         </div>
       </div>
