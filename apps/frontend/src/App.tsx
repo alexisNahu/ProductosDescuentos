d8596f8c39a6217a3040f03dd3e83ec290f92d05
anig <alexisnahuelidoyaga@gmail.com>
2025-05-27 15:19:11 -0300

    agregado componente personal tarea 5

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,11 +1,21 @@
 import { Sidebar } from './components'
 import { sidebarItems } from './components/Sidebar/sidebar.items'
+import AlexisComponent from './components/AlexisComponent/AlexisComponent'
 
 
 function App() {
   return (
     <>
-      <Sidebar items = {sidebarItems} />
+      <div className='container-fluid p-0'>
+        <div className="row">
+          <div className="col">
+            <Sidebar items = {sidebarItems} />
+          </div>
+          <div className="col">
+            <AlexisComponent a={2} b={2}/>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
